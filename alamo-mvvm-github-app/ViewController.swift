a86e86f8ef430ee1ff4bef2054519e5042e5fafc
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-12-14 07:36:33 +0900

    First commit

---
+++ alamo-mvvm-github-app/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  
+//  
+//  Created by Seigetsu on 2023/12/14
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
