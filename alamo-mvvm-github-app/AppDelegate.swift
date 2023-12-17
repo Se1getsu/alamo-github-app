9f17c537a20a0932c0e0701e697b1894f2d09815
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-12-17 10:32:56 +0900

    change: ナビゲーションバーの色をカスタマイズした

--- alamo-mvvm-github-app/AppDelegate.swift
+++ alamo-mvvm-github-app/AppDelegate.swift
@@ -14,6 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Navigation bar customize
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.backgroundColor = .systemIndigo.withAlphaComponent(0.3)
+        UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+        UINavigationBar.appearance().compactAppearance = navigationBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.label]
+        UINavigationBar.appearance().tintColor = .systemBlue
+        
         // Override point for customization after application launch.
         return true
     }
